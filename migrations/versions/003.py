20e24a72bcc1ff71eb036d42f555f887b42bd411
Alejandro Carmona Reina <alecarrei1@alum.us.es>
2025-11-09 12:02:01 +0100

    fix: Migrations
    Co-authored-by: Darío Román Jiménez  <dariorojime@gmail.com>
    Co-authored-by: Samuel Granado Oliva  <samuelgranadooliva36@gmail.com>

--- migrations/versions/003.py
+++ migrations/versions/003.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '05e898d99ea2'
-down_revision = '17f1be643b26'
+down_revision = '53250b584934'
 branch_labels = None
 depends_on = None
 
