5c4f349428b7506707bdf264ebb0c3c5bc2f10e6
Samuel Granado Oliva <samuelgranadooliva36@gmail.com>
2025-12-16 16:49:35 +0100

    feat: Added communities screen

--- app/modules/community/routes.py
+++ app/modules/community/routes.py
@@ -1,7 +1,6 @@
-from flask import jsonify
+from flask import jsonify, render_template
 from app.modules.community import community_bp
 from app.modules.community.models import Community
-
 
 @community_bp.route("/api/communities", methods=["GET"])
 def list_communities():
@@ -11,3 +10,10 @@ def list_communities():
         "name": c.name,
         "logo_url": c.logo_url
     } for c in communities]), 200
+    
+@community_bp.route("/communities", methods=["GET"])
+def view_communities():
+    communities = Community.query.order_by(Community.name.asc()).all()
+    return render_template('list_communities.html', communities=communities)
+
+
